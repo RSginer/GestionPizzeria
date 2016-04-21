f26dc96a9e694697ff7c4978a45cf1c091670cf2
r.solerginer@gmail.com <>
2016-04-21 13:23:57 +0200

    pizzeria

---
+++ pizzeriaCalzoneWTF_3/src/pizzeria/Pizza.java
@@ -0,0 +1,164 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pizzeria;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.DecimalFormat;
+import java.time.LocalDate;
+import java.util.List;
+
+/**
+ *
+ * @author daw1
+ */
+public class Pizza {
+
+    double precioTipoPizza;
+    double precioIngredientes = 0;
+    double total;
+    String totalFormat;
+    double masa;
+    String tipo;
+    String tamaño;
+    List<String> listaIngredientes;
+
+    Pizza(double masa, String tipo, String tamaño, List listaIngredientes) {
+        this.masa = masa;
+        this.tipo = tipo;
+        this.tamaño = tamaño;
+        this.listaIngredientes = listaIngredientes;
+    }
+
+    public double calculaIngredientes() {
+        this.precioIngredientes = 0;
+        for (String ingrediente : listaIngredientes) {
+            switch (ingrediente) {
+                case "Jamon":
+                    this.precioIngredientes += 0.5;
+                    break;
+                case "Olivas":
+                    this.precioIngredientes += 1;
+                    break;
+                case "Cebolla":
+                    this.precioIngredientes += 2.5;
+                    break;
+                case "Tomate":
+                    this.precioIngredientes += 1.5;
+                    break;
+                case "Queso":
+                    this.precioIngredientes += 0.75;
+                    break;
+            }
+        }
+        return this.precioIngredientes;
+    }
+
+    public void setListaIngredientes(List<String> listaIngredientes) {
+        this.listaIngredientes = listaIngredientes;
+    }
+
+    public double calculaTipo() {
+        switch (this.tipo) {
+            case "Basica": {
+                precioTipoPizza = 3;
+                break;
+            }
+            case "Cuatro Quesos": {
+                precioTipoPizza = 5;
+                break;
+            }
+            case "Barbacoa": {
+                precioTipoPizza = 7;
+                break;
+            }
+            case "Mexicana": {
+                precioTipoPizza = 8.5;
+                break;
+            }
+        }
+        return precioTipoPizza;
+    }
+    public void calcularPrecio() {
+        switch (this.tamaño) {
+            case "Pequeña":
+                this.total = (this.masa + this.precioTipoPizza + this.precioIngredientes);
+                break;
+            case "Mediana":
+                this.total = (this.masa + this.precioTipoPizza + this.precioIngredientes) * 1.15;
+                break;
+            case "Familiar":
+                this.total = (this.masa + this.precioTipoPizza + this.precioIngredientes) * 1.30;
+                break;
+        }
+        DecimalFormat format = new DecimalFormat("##.##");
+        this.totalFormat = format.format(this.total) + "";
+    }
+
+    public double getPrecioTipoPizza() {
+        return precioTipoPizza;
+    }
+
+    public void setPrecioTipoPizza(double precioTipoPizza) {
+        this.precioTipoPizza = precioTipoPizza;
+    }
+
+    public double getPrecioIngredientes() {
+        return precioIngredientes;
+    }
+
+    public void setPrecioIngredientes(double precioIngredientes) {
+        this.precioIngredientes = precioIngredientes;
+    }
+
+    public double getMasa() {
+        return masa;
+    }
+
+    public void setMasa(double masa) {
+        this.masa = masa;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getTamaño() {
+        return tamaño;
+    }
+    public void imprimirTicket(int contador) throws IOException{
+        contador++;
+        String nombreticket = "ticket-" + contador + ".txt";
+        FileWriter ticket  = new FileWriter(nombreticket,true);
+        PrintWriter impresora = new PrintWriter(ticket);
+        impresora.println("mi polla morena");
+    }
+    
+    public void setTamaño(String tamaño) {
+        this.tamaño = tamaño;
+    }
+    public String pedido(){
+        LocalDate hoy = LocalDate.now();
+        String masa = null;
+        String ingredientes= null;
+    if(this.masa==9){
+        masa = "Normal";
+    }else if(this.masa==9.5){
+        masa = "Integral";
+    }
+    if(this.listaIngredientes.isEmpty() | this.listaIngredientes.contains("Sin ingrediente extra")){
+        ingredientes="Ninguno";
+    }else{
+        ingredientes =  this.listaIngredientes.toString();
+    }
+    String texto="PIZZERIA MISLATA - "  + hoy + "\n" + "-----------------------------------------" + "\n" + "Masa: "  + " " + this.masa + "€ " + masa +  "\n" + "Tipo: " + " " + this.precioTipoPizza + " " + this.tipo +  "\n" + "Ingredientes extra: " + " " + this.precioIngredientes+"€ - " + ingredientes + "\n" + "Tamaño: " + this.tamaño + "\n" + "TOTAL: " + this.totalFormat;
+    return texto;}
+}
